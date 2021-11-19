bb1bb3697ba718c6a8cf4b3bddf061c841ae9eea
Guido D'Orsi <guido.dorsi@immobiliare.it>
2021-11-19 18:46:31 +0100

    chore: made easier to change runtime

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const fs = require("fs");
-const loader = require("@assemblyscript/loader");
-const wasmModule = loader.instantiateSync(
-  fs.readFileSync(__dirname + "/build/optimized.wasm")
-);
-
-const { __newString, __pin } = wasmModule.exports;
+const defaultMatcherModule = require("./matcher/optimized-wasm");
 
 class Router {
   methods = {};
 
+  constructor(matcherModule = defaultMatcherModule) {
+    this.matcherModule = matcherModule;
+  }
+
   on(method, url, callback) {
     if (!this.methods[method]) {
-      const matcher = wasmModule.exports.create();;
-
-      __pin(matcher);
-
       this.methods[method] = {
-        matcher,
+        matcher: this.matcherModule.create(),
         routes: [],
       };
     }
@@ -25,20 +19,20 @@ class Router {
     const id = router.routes.length;
 
     router.routes.push({ url, callback });
-    wasmModule.exports.add(router.matcher, __newString(url), id);
+    this.matcherModule.add(router.matcher, url, id);
   }
 
   lookup({ method, url }) {
     const router = this.methods[method];
 
     if (router === undefined) {
-        return;
+      return;
     }
 
-    const id = wasmModule.exports.match(router.matcher, __newString(url));
+    const id = this.matcherModule.match(router.matcher, url);
 
     if (id === -1) {
-        return;
+      return;
     }
 
     const route = router.routes[id];
---
+++ matcher/optimized-wasm.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const loader = require("@assemblyscript/loader");
+const imports = {
+  /* imports go here */
+};
+const wasmModule = loader.instantiateSync(
+  fs.readFileSync(__dirname + "/../build/optimized.wasm"),
+  imports
+);
+
+const { __newString, __pin } = wasmModule.exports;
+
+function create() {
+  const matcher = wasmModule.exports.create();
+
+  __pin(matcher);
+
+  return matcher;
+}
+
+function add(routes, route, id) {
+  return wasmModule.exports.add(routes, __newString(route), id);
+}
+
+function match(routes, url) {
+  return wasmModule.exports.match(routes, __newString(url));
+}
+
+module.exports = {
+  add,
+  create,
+  match,
+};
